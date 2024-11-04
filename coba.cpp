23466260c613dd4b9af902a41bab6f610a2e4dc9
DimasFebryansyah <dimasfebry555@gmail.com>
2024-11-04 12:04:10 +0700

    4

--- coba.cpp
+++ coba.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
-void pertambahan;
+void pertambahan()
+{
+    int angka1, angka2;
+    cout << "Selamat datang di pertambahan" << endl;
+    cout << "Masukkan Angka pertama : ";
+    cin >> angka1;
+    cout << "Masukkan Angka kedua : ";
+    cin >> angka2;
+    
+};
 
-int main(){
-    int pilih
+int main()
+{
+    int pilih;
+    cout << "Selamat Datang" << endl;
+    cout
 }
